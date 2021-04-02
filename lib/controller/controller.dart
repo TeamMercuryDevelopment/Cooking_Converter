879869cb7af7bcb53952afe238236d89109819dd
TeamMercury <80074708+TeamMercuryDevelopment@users.noreply.github.com>
2021-04-02 15:08:03 -0300

    🛠 Project Organization.

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,25 +1,36 @@
 import 'package:mobx/mobx.dart';
 
-import '../convert.dart';
-import '../product.dart';
+import '../models/convert.dart';
+import '../models/product.dart';
 part 'controller.g.dart';
 
 class Controller = _ControllerBase with _$Controller;
 
 abstract class _ControllerBase with Store {
-
   @observable
   ObservableList<Product> listProduct = [
-    Product(name: "Item 1",),
-    Product(name: "Item 2",),
-    Product(name: "Item 3",),
+    Product(
+      name: "Item 1",
+    ),
+    Product(
+      name: "Item 2",
+    ),
+    Product(
+      name: "Item 3",
+    ),
   ].asObservable();
 
   @observable
   ObservableList<Convert> listConvert = [
-    Convert(name: "Conversão 1",),
-    Convert(name: "Conversão 2",),
-    Convert(name: "Conversão 3",),
+    Convert(
+      name: "Conversão 1",
+    ),
+    Convert(
+      name: "Conversão 2",
+    ),
+    Convert(
+      name: "Conversão 3",
+    ),
   ].asObservable();
 
   @observable
